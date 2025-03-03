0dfc35bd453c794f0212198df6988fbdc6365e8f
sangan007 <sangeerthsangan107@gmail.com>
2025-03-03 16:33:07 +0530

    app commit

--- app.py
+++ app.py
@@ -200,4 +200,5 @@ def analyze_audio(audio_path):
     return report
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT if it's set, otherwise default to 5000
+    app.run(host="0.0.0.0", port=port)
